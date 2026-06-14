a7d870ed236b02f14338a44686337cbef59e346f
Rose2999 <roseedacherry29@gmail.com>
2026-06-14 20:16:06 +0530

    feature/file_upload:profile image upload using multer

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 const employeeRoutes = require("./routes/employee.routes");
+const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.get("/", (req, res) => {
   res.json({
     success: true,
